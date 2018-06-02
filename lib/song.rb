dce22561be1c5d0f264a41f8bff551232092a40c
Scott Silverman <scottms724@gmail.com>
2018-06-02 15:49:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @title = title 
   end 
   
-  def self.new_by_filename(filename)
+  def self.new_by_filename(file)
     song = self.new
     song.title = filename.split(" - ")[1]
     song.artist_name = artist_name 
