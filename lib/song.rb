599098d421cad2f1bcea2865475265b725bf68fc
Scott Silverman <scottms724@gmail.com>
2018-06-02 16:22:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.new_by_filename(file)
     song_parsed = file.chomp(".mp3").split(" - ")
     song = self.new(song_parsed[1])
-    song.artist_name = song_parsed[2]
+    song.artist_name = song_parsed[0]
     song 
   end 
   
